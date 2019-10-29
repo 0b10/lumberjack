c9b12bd0832d8bd322342e8be42f79b5415b3ac6
0b10 <0b10@pm.me>
2019-10-29 14:01:21 +0000

    Add comment

--- dist/bin/configs/pino.js
+++ dist/bin/configs/pino.js
@@ -6,6 +6,7 @@
 const logger = require("pino")({
     enabled: true,
     timestamp: true,
+    // set to max, and control via lumberjack LOG_LEVEL env
     level: "trace",
     customLevels: {
         // trace: 10, debug: 20, info: 30, warn: 40, error: 50, critical: 55, fatal: 60, silent: infinity
--- src/bin/configs/pino.js
+++ src/bin/configs/pino.js
@@ -8,8 +8,12 @@
 
 const logger = require("pino")({
   enabled: true,
+
   timestamp: true,
+
+  // set to max, and control via lumberjack LOG_LEVEL env
   level: "trace",
+
   customLevels: {
     // trace: 10, debug: 20, info: 30, warn: 40, error: 50, critical: 55, fatal: 60, silent: infinity
     critical: 55,
